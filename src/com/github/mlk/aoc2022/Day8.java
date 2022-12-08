669a76edbb3f9775197cb364614ec1b7ff6c947c
Michael Lee <michael@iwritecode.michael-lloyd-lee.me.uk>
2022-12-08 13:05:46 +0000

    AoC-2022: Day 8 pt2

--- src/com/github/mlk/aoc2022/Day8.java
+++ src/com/github/mlk/aoc2022/Day8.java
@@ -110,39 +110,94 @@ public class Day8 {
             101211324120322333355055001035515221630532526633042003421442155144120444232152535003241233413001002""";
 
     public static void main(String... arg) {
-        String[] lines = testData.split("\n");
+        String[] lines = data.split("\n");
         int[][] trees = new int[lines.length][lines[0].length()];
-        System.out.println(trees.length);
-        System.out.println(trees[0].length);
         for(int x = 0; x< lines.length; x++) {
             char[] currentLine = lines[x].toCharArray();
             for(int y = 0; y< currentLine.length; y++) {
                 trees[x][y] = Integer.parseInt(currentLine[y] + "");
             }
         }
-        int counter = 0;
-        for(int x = 0; x< trees.length; x++) {
-            for(int y = 0; y< trees[x].length; y++) {
-                if(isVisibleL(trees, x, y) || isVisibleR(trees, x, y) || isVisibleD(trees, x, y) || isVisibleU(trees, x, y)) {
-                    counter++;
-                    System.out.print(trees[x][y]);
-                } else {
-                    System.out.print(" ");
+
+        int highScore = 0;
+        for(int x = 1; x< trees.length-1; x++) {
+            for(int y = 1; y< trees[x].length-1; y++) {
+                if(score(trees, x, y) > highScore) {
+                    highScore = score(trees, x, y);
                 }
             }
-            System.out.println();
+
         }
-        System.out.println(counter);
+        System.out.println(highScore);
     }
 
-    static boolean score(int[][] trees, int x, int y) {
+    static int score(int[][] trees, int x, int y) {
+
+        int total = 0;
+
+        int l = scoreL(trees, x, y);
+        total = l;
+        //System.out.println("L: " + l + " T " + total);
+
+        int r = scoreR(trees, x, y);
+        total = l * r;
+        //System.out.println("R: " + r + " T " + total);
+
+        int u = scoreU(trees, x, y);
+        total = l * r * u;
+        //System.out.println("U: " + u + " T " + total);
+
+        int d = scoreD(trees, x, y);
+        total = l * r * u * d;
+        //System.out.println("D: " + d + " T " + total);
+
+        return total;
+    }
+
+    static int scoreL(int[][] trees, int x, int y) {
         int score = 0;
         for(int i = x-1; i >= 0; i--) {
+            score++;
             if(trees[x][y] <= trees[i][y]) {
-
+               break;
             }
         }
-        return lowest;
+        return score;
+    }
+
+    static int scoreD(int[][] trees, int x, int y) {
+        int score = 0;
+        for(int i = y-1; i >= 0; i--) {
+            score++;
+            if(trees[x][y] <= trees[x][i]) {
+                break;
+            }
+        }
+
+        return score;
+    }
+
+    static int scoreR(int[][] trees, int x, int y) {
+        int score = 0;
+        for(int i = x+1; i < trees.length; i++) {
+            score++;
+            if(trees[x][y] <= trees[i][y]) {
+                break;
+            }
+        }
+        return score;
+    }
+
+    static int scoreU(int[][] trees, int x, int y) {
+        int score = 0;
+        for(int i = y+1; i < trees.length; i++) {
+            score++;
+            if(trees[x][y] <= trees[x][i]) {
+                break;
+            }
+        }
+
+        return score;
     }
 
     static boolean isVisibleL(int[][] trees, int x, int y) {
