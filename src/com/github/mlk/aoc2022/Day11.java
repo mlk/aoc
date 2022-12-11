ebb6b74f1acf99924d6703e3867bde184831a51b
Michael Lee <michael@iwritecode.michael-lloyd-lee.me.uk>
2022-12-11 13:41:46 +0000

    AoC-2022: Day 11 pt2

--- src/com/github/mlk/aoc2022/Day11.java
+++ src/com/github/mlk/aoc2022/Day11.java
@@ -5,6 +5,7 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 import java.util.*;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -98,8 +99,8 @@ public class Day11 {
 
     static ScriptEngineManager factory = new ScriptEngineManager();
     static ScriptEngine engine = factory.getEngineByName("JavaScript");
-
-    record Monkey(int number, List<Integer> items, Function<Integer, Integer> operation, int divisibleTest, int trueMonkey, int falseMonkey) {
+    static int totalMonkeys = 8;
+    record Monkey(int number, List<int[]> items, Function<Integer, Integer> operation, int divisibleTest, int trueMonkey, int falseMonkey) {
         static Monkey parse(String monkey) throws ScriptException {
             String[] lines = monkey.split("\n");
             int number = Integer.parseInt(lines[0].substring(7, lines[0].length() - 1).trim());
@@ -108,15 +109,18 @@ public class Day11 {
             int falseMonkey = Integer.parseInt(lines[5].substring(29).trim());
             String calc = lines[2].substring(18);
 
-            List<Integer> items = Arrays.stream(lines[1].substring(18).split(",")).map(String::trim).map(Integer::parseInt).toList();
+            List<int[]> items = Arrays.stream(lines[1].substring(18).split(",")).map(String::trim).map(Integer::parseInt).map((x) -> {
+                int[] values = new int[totalMonkeys];
+                Arrays.fill(values, x);
+                return values;
+            }).toList();
 
             Function<Integer, Integer> operation = (oldValue) -> {
-
 
                 engine.put("old", oldValue);
 
                 try {
-                    return ((Number) engine.eval("Math.floor( (Math.floor(" + calc + ")) / 3)")).intValue();
+                    return ((Number) engine.eval("Math.floor( " + calc + ") ")).intValue();
                 } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
@@ -133,15 +137,20 @@ public class Day11 {
             }
 
             int[] monkeyInspections = new int[monkeys.size()];
-            for(int i = 0; i< 20; i++) {
+            for(int i = 0; i< 10000; i++) {
                 for(Monkey monkey : monkeys) {
-                    for(int item : monkey.items()) {
+
+                    for(int[] item : monkey.items()) {
                         monkeyInspections[monkey.number]++;
-                        int worry = monkey.operation().apply(item);
-                        if(worry % monkey.divisibleTest() == 0) {
-                            monkeys.get(monkey.trueMonkey()).items().add(worry);
+
+                        for(int itemMonkey = 0; itemMonkey<monkeys.size(); itemMonkey++) {
+                            item[itemMonkey] = monkey.operation().apply(item[itemMonkey]) % monkeys.get(itemMonkey).divisibleTest();
+                        }
+
+                        if(item[monkey.number] == 0) {
+                            monkeys.get(monkey.trueMonkey()).items().add(item);
                         } else {
-                            monkeys.get(monkey.falseMonkey()).items().add(worry);
+                            monkeys.get(monkey.falseMonkey()).items().add(item);
                         }
                     }
                     monkey.items().clear();
@@ -151,7 +160,7 @@ public class Day11 {
                 System.out.println(i + " " + monkeyInspections[i]);
             }
             int[] sorted = Arrays.stream(monkeyInspections).sorted().toArray();
-            System.out.println(sorted[sorted.length-1] * sorted[sorted.length-2]);
+            System.out.println(((long)sorted[sorted.length-1]) * sorted[sorted.length-2]);
         }
 
     }
