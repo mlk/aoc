9835999aa2f0f2da462040fb1dc8eeb672879b1f
Michael Lee <michael@iwritecode.michael-lloyd-lee.me.uk>
2022-12-13 09:44:11 +0000

    AoC-2022: Day 13pt2

--- src/com/github/mlk/aoc2022/Day13.java
+++ src/com/github/mlk/aoc2022/Day13.java
@@ -5,7 +5,11 @@ import org.openjdk.nashorn.api.scripting.ScriptObjectMirror;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Day13 {
     static String input = """
@@ -457,7 +461,10 @@ public class Day13 {
             [[[2,8,1,[3,5,2,1]]],[[[5],9,1,[10,1,4,0,6]],4],[[7,[],4,[7]],3,8,0,[]],[8,[[4,5,10],4],[[]]],[0,[8,4,2,[],3],5]]
                         
             [[0,[],2,10,8],[[]],[7,[[7,10],6,2,[5,10,9,7,6],[8,8,2]],9],[6,5,[7,0,10,[]]]]
-            [[[[7],[7,10,9,1],[9,1,0,1,9],[9]],[2,3,[8,1,9],9],7,7,7],[[6]],[3,1,1,[[6,9],[7,6,0]],10]]""";
+            [[[[7],[7,10,9,1],[9,1,0,1,9],[9]],[2,3,[8,1,9],9],7,7,7],[[6]],[3,1,1,[[6,9],[7,6,0]],10]]
+            
+            [[2]]
+            [[6]]""";
 
     static String testInput = """
             [1,1,3,1,1]
@@ -482,60 +489,83 @@ public class Day13 {
             [[]]
                         
             [1,[2,[3,[4,[5,6,7]]]],8,9]
-            [1,[2,[3,[4,[5,6,0]]]],8,9]""";
+            [1,[2,[3,[4,[5,6,0]]]],8,9]
+            
+            [[2]]
+            [[6]]""";
 
     static ScriptEngineManager factory = new ScriptEngineManager();
     static ScriptEngine engine = factory.getEngineByName("JavaScript");
 
-    public static void main(String... arg) throws ScriptException {
-        String[] pairs = input.split("\n\n");
-        int index = 1;
-        int total = 0;
-        for(String pair : pairs) {
-            String[] inputs = pair.split("\n");
-            Object left = engine.eval(inputs[0]);
-            Object right = engine.eval(inputs[1]);
+    static Object eval(String data) {
+        try {
+            return engine.eval(data);
+        } catch (ScriptException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-            if(inOrder(left, right) == Order.IN) {
-                total += index;
+    public static void main(String... arg) throws ScriptException {
+
+        List<Object> ordered = Arrays.stream(input.split("\n")).filter(x -> !x.isBlank())
+                .map(Day13::eval)
+                .sorted((x, y) -> inOrder(y, x).ordering)
+
+                .toList();
+        int index = 1;
+        int total = 1;
+        for (Object i : ordered) {
+            ScriptObjectMirror item = (ScriptObjectMirror) i;
+            if(item.size() == 1 && item.getSlot(0) instanceof ScriptObjectMirror first && first.size() == 1 &&
+                    (first.getSlot(0).equals(2) || first.getSlot(0).equals(6))) {
+                total *= index;
             }
             index++;
         }
         System.out.println(total);
     }
 
-    static Order inOrder(Object left, Object right) throws ScriptException {
-        if(left instanceof Integer && right instanceof Integer) {
-            if((Integer) left < (Integer) right) {
-                return Order.IN;
-            } else if ((Integer) left > (Integer) right) {
-                return Order.OUT;
-            } else {
-                return Order.CONTINUE;
-            }
-        }
-        if(left instanceof ScriptObjectMirror leftM && right instanceof ScriptObjectMirror rightM) {
-            int minLength = Math.min(leftM.size(), rightM.size());
-            for(int i = 0; i<minLength; i++) {
-                Order currentValue = inOrder(leftM.getSlot(i), rightM.getSlot(i));
-                if(currentValue != Order.CONTINUE) {
-                    return currentValue;
+    static Order inOrder(Object left, Object right) {
+        try {
+            if (left instanceof Integer && right instanceof Integer) {
+                if ((Integer) left < (Integer) right) {
+                    return Order.IN;
+                } else if ((Integer) left > (Integer) right) {
+                    return Order.OUT;
+                } else {
+                    return Order.CONTINUE;
                 }
             }
-            if(leftM.size() < rightM.size()) {
-                return Order.IN;
-            } else if(leftM.size() > rightM.size()) {
-                return Order.OUT;
+            if (left instanceof ScriptObjectMirror leftM && right instanceof ScriptObjectMirror rightM) {
+                int minLength = Math.min(leftM.size(), rightM.size());
+                for (int i = 0; i < minLength; i++) {
+                    Order currentValue = inOrder(leftM.getSlot(i), rightM.getSlot(i));
+                    if (currentValue != Order.CONTINUE) {
+                        return currentValue;
+                    }
+                }
+                if (leftM.size() < rightM.size()) {
+                    return Order.IN;
+                } else if (leftM.size() > rightM.size()) {
+                    return Order.OUT;
+                }
+                return Order.CONTINUE;
+            } else if (left instanceof Integer && right instanceof ScriptObjectMirror) {
+                return inOrder(engine.eval("[" + left + "]"), right);
+            } else {
+                return inOrder(left, engine.eval("[" + right + "]"));
             }
-            return Order.CONTINUE;
-        } else if(left instanceof Integer && right instanceof ScriptObjectMirror) {
-            return inOrder(engine.eval("[" + left + "]"), right);
-        } else {
-            return inOrder(left, engine.eval("[" + right + "]"));
+        } catch(ScriptException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    enum Order { IN, OUT, CONTINUE
+    enum Order { IN(1), OUT(-1), CONTINUE(0);
+        final int ordering;
+
+        Order(int ordering) {
+            this.ordering = ordering;
+        }
     }
 
 }
