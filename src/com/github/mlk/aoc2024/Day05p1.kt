c0fa07a53f47265bd5ea41a02d7ab2f43e2b6f87
Michael Lee <michael.lloyd.lee@gmail.com>
2024-12-10 22:41:36 +0000

    AOC-2024: Day 5p1

--- src/com/github/mlk/aoc2024/Day05p1.kt
+++ src/com/github/mlk/aoc2024/Day05p1.kt
@@ -3,21 +3,23 @@ package com.github.mlk.aoc2024
 import java.io.File
 
 fun main() {
-    val forwards = mutableMapOf<Int, MutableList<Int>>()
-    val backwards = mutableMapOf<Int, MutableList<Int>>()
-    val data = File("/home//mlk/input.txt").readLines()
-    data.filter { it.contains('|') }.forEach { line ->
-        val pairing = line.split("|").map { it.toInt() }
-        val f = forwards.getOrPut(pairing[0]) { mutableListOf() }
-        val b = backwards.getOrPut(pairing[1]) { mutableListOf() }
-        f.add(pairing[1])
-        b.add(pairing[0])
+    val data = File("/home/mlk/input.txt").readLines()
+    val ordering = data.filter { it.contains('|') }.map { line ->
+        val l = line.split("|")
+        Pair(l[0].toInt(), l[1].toInt())
     }
-    data.filter { it.contains(",") }.filter { line ->
-        val pages = line.split(",").map { it.toInt() }
-        pages.forEachIndexed { i, num ->
-            
-        }
-        true
-    }
+    println(data.filter { it.contains(",") }
+        .map { line -> line.split(",").map { it.toInt() } }
+        .filter { pages ->
+            pages.forEachIndexed { i, v ->
+                for (otherIndex in 0..i) {
+                    if (ordering.contains(Pair(v, pages[otherIndex]))) {
+                        return@filter false
+                    }
+                }
+            }
+            true
+        }.sumOf {
+            it[it.size / 2]
+        })
 }
