9cbcb8992c172c58907b839d51dd8ab98e430968
Micahel Lee <Michael.lloyd.lee@gmail.com>
2025-12-03 08:13:45 +0000

    AOC-2018: Day 3

---
+++ src/com/github/mlk/aoc2018/Day3p1.kt
@@ -0,0 +1,19 @@
+package com.github.mlk.aoc2018
+
+import java.io.File
+
+fun main() {
+    val data = File("C:\\Users\\SR116\\Downloads\\input.txt").readText()
+    println(
+        data.split("\n")
+            .map { it.trim() }
+            .filter { !it.isEmpty() }.sumOf { currrentNumber ->
+
+                val numbers = currrentNumber.toCharArray().map { it.digitToInt() }.toList()
+                val maxPair =
+                    numbers.subList(0, numbers.size - 1).mapIndexed { index, i -> Pair(index, i) }.maxBy { it.second }
+                val secondMax = numbers.subList(maxPair.first + 1, numbers.size).max()
+                maxPair.second * 10 + secondMax
+            }
+    )
+}
---
+++ src/com/github/mlk/aoc2018/Day3p2.kt
@@ -0,0 +1,25 @@
+package com.github.mlk.aoc2018
+
+import java.io.File
+
+fun main() {
+    val data = File("C:\\Users\\SR116\\Downloads\\input.txt").readText()
+    println(
+        data.split("\n")
+            .map { it.trim() }
+            .filter { !it.isEmpty() }.sumOf { currrentNumber ->
+
+                val numbers = currrentNumber.toCharArray().map { it.digitToInt() }.toList()
+                val numList = mutableListOf<Int>()
+                var startPoint = 0
+                for (i in 11 downTo 0) {
+                    val maxPair =
+                        numbers.subList(startPoint, numbers.size - i).mapIndexed { index, i -> Pair(index, i) }
+                            .maxBy { it.second }
+                    numList.add(maxPair.second)
+                    startPoint = maxPair.first + 1 + startPoint
+                }
+                numList.joinToString("") { it.toString() }.toULong()
+            }
+    )
+}
